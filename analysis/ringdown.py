2e8143cfa6211319a8a94ad3dcdc09c111672e91
Kelvin <zcapkkk@ucl.ac.uk>
2024-08-06 12:00:20 +0200

    tests passed, ringdown class should work now

--- analysis/ringdown.py
+++ analysis/ringdown.py
@@ -4,7 +4,6 @@ import matplotlib.gridspec as gridspec
 from dataclasses import dataclass, field
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
-from uncertainties.umath import log
 
 plt.style.use('seaborn-v0_8-whitegrid')
 
@@ -21,11 +20,16 @@ class Ringdown:
     cropnormtimetrace: np.ndarray = field(repr=False, init=False)
     logtimetrace: np.ndarray = field(repr=False, init=False)
 
-    def __post_init__(self):
-        # ensure that input is numpy array
-        pass
+    tau: float = field(default=0., init=False) # decay time
+    tau_err: float = field(default=0, init=False, repr=False)
 
-    def fit(self, t0=None, window=None, p0=[0.2, 1e-6, 0.], plot=True,):
+    def __post_init__(self):
+        popt, pcov = self.fit()
+        self.tau = popt[1]
+        self.tau_err = np.sqrt(np.diag(pcov))[1]
+
+
+    def fit(self, t0=None, window=None, p0=[0.2, 1e-6, 0.], plot=False,):
         '''Given t0 and a window, 
         1. crops time trace
         2. takes the log
@@ -49,7 +53,6 @@ class Ringdown:
         residuals = self.logtimetrace - self.fit_func(self.t_crop, *popt)
         # calculate rmse
         rmse = np.sqrt(np.sum(residuals*residuals)/(len(self.logtimetrace)-3))
-        print(f"rmse: {rmse}")
 
         ### EDIT LATER
         if plot:
@@ -64,7 +67,11 @@ class Ringdown:
         returns cropped time, a mask, 
         the cropped and normalized timetrace and log of timetrace'''
         mask = np.logical_and(self.t >= t0, self.t <= t0+window)
-        timetrace = self.timetrace[mask] - np.min(self.timetrace) + offset
+        # check whether any value in the *cropped* timetrace is less than zero
+        # if yes offset the timetrace, because otherwise it messes up fit
+        timetrace = self.timetrace[mask]
+        if np.any(timetrace) <= 0:
+            timetrace -= np.min(timetrace) + offset
         cropnormtimetrace = timetrace / np.max(timetrace)
         logtimetrace = np.log(cropnormtimetrace)
         t_crop = self.t[mask]
@@ -136,7 +143,7 @@ class Ringdown:
         residual = self.fit_func(self.t_crop, *popt) - self.logtimetrace
         mean_res = np.mean(residual)
         std_res = np.std(residual, ddof=1)
-        ax3.plot(self.t_crop/1e-6, residual, marker='.', markersize=1, ls='', color='tab:red')
+        ax3.plot(self.t_crop/1e-6, residual, marker='.', markersize=1, alpha=0.8, ls='', color='tab:red')
         ax3.axhspan(mean_res + std_res, mean_res - std_res, fill=0, ls='--', color='black', label=f"$\\sigma:$ {std_res:.1e}")
         ax3.legend(frameon=True, facecolor='white', loc='lower left', markerscale=10)
         ax3.set_ylabel("Residual")
@@ -157,50 +164,10 @@ class Ringdown:
         residual = self.cropnormtimetrace - (popt[0]*np.exp(-self.t_crop/popt[1])+popt[2])
         mean_res = np.mean(residual)
         std_res = np.std(residual, ddof=1)
-        ax5.plot(self.t_crop/1e-6, residual, marker='.', ls='', markersize=1, color='tab:green')
+        ax5.plot(self.t_crop/1e-6, residual, marker='.', ls='', markersize=1, alpha=0.8, color='tab:green')
         ax5.axhspan(mean_res + std_res, mean_res - std_res, fill=0, ls='--', color='black', label=f"$\\sigma:$ {std_res:.1e}")
         ax5.legend(frameon=True, facecolor='white', loc='lower left', markerscale=10)
         ax5.set_ylabel("Residual")
         ax5.set_xlabel("Time ($\\mu$s)")
         
         plt.show()
-
-
-#
-#        plt.subplot(121)
-#        plt.plot(self.t, self.timetrace)
-#        plt.subplot(222)
-#        plt.plot(t_crop, logtimetrace)
-#        plt.plot(t_crop, self.fit_func(t_crop, *popt))
-#        plt.subplot(224)
-#        plt.plot(t_crop, self.cropnormtimetrace)
-#        plt.plot(t_crop, popt[0]*np.exp(-t_crop/popt[1])+popt[2])
-#        plt.legend()
-#        plt.tight_layout()
-#        plt.show()
-        
-def main():
-    t = np.arange(0, 2e-6, 2.5e-10)
-    a, tau, c = [0.4, 1.8e-6, 0.03]
-    trace_og = a*np.exp(-t/tau) - c
-    noise_sd = a/100
-    noise = np.random.normal(0, noise_sd, len(trace_og))
-    trace = trace_og + noise
-    ringdown = Ringdown(timetrace=trace, t=t)
-    print(f"injected noise: {noise_sd}")
-    print(f"actual noise sd: {np.std(noise, ddof=1)}")
-    print(f"denoise signal and recalculate:\n mean: {np.mean(trace-trace_og)}, std: {np.std(trace - trace_og, ddof=1)}")
-
-    popt, pcov = ringdown.fit(window=2e-6)
-    delta_a, delta_tau, delta_c = np.sqrt(np.diag(pcov))
-
-
-
-
-if __name__ == '__main__':
-    main()
-
-
-
-        
-
--- test/test_ringdown.py
+++ test/test_ringdown.py
@@ -9,14 +9,13 @@ from analysis.ringdown import Ringdown
 class TestRingdown(unittest.TestCase):
 
     def test_fit(self):
-        t = np.arange(0, 2e-6, 2.5e-10)
+        '''Test to see whether we can recover the correct time decay constant'''
         a, tau, c = [0.4, 1.8e-6, 0.03]
-        trace = a*np.exp(-t/tau) - c
-        trace += np.random.normal(0, a/5, len(trace))
+        t, trace = self.generate_timetrace(a, tau, c, a/100 )
 
         ringdown = Ringdown(timetrace=trace, t=t)
 
-        popt, pcov = ringdown.fit(plot=False)
+        popt, _ = ringdown.fit(plot=False)
 
         # test if the fitted parameters are almost wqual
 
@@ -24,7 +23,27 @@ class TestRingdown(unittest.TestCase):
                          fit is {popt[1]} while tau is {tau}."
         self.assertAlmostEqual(tau, popt[1], 4, message_tau)
 
+    def test_noise(self):
+        '''Test to see whether the correct noice statistic can be recovered'''
+        a, tau, c = [0.8, 1.2e-6, 0.]
+        t, trace = self.generate_timetrace(a, tau, c, a/80)
 
+        ringdown = Ringdown(timetrace=trace, t=t)
+
+        std = ringdown.exp_residual()
+
+        self.assertAlmostEqual(std*np.max(ringdown.timetrace), a/80, 2, "Noise statistic does not fit.")
+        
+        
+
+    def generate_timetrace(self, a, tau, c, noise_sd, tEnd=2e-6, tInc=2.5e-10):
+        '''Generates a timetrace of an exponential with a, tau, c
+        and sprinkle in a normally distributed noise with a noise_sd.'''
+        t = np.arange(0, tEnd, tInc)
+        trace = a*np.exp(-t/tau) + c
+        trace += np.random.normal(0, noise_sd, len(trace))
+
+        return t, trace
 
 
 if __name__ == '__main__':

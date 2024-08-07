33dad09449bbedd40bbfd89c9d4ff195aa9822fe
Kelvin <zcapkkk@ucl.ac.uk>
2024-08-07 16:59:16 +0200

    push to github

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from loading import Experiment
-from analysis.ringdowns import Ringdowns
-from analysis.ringdown import Ringdown
+from analysis.loading import Experiment
 
 def main():
     folder_path = '/home/kelvin/LabInnsbruck/WindowsData/20240715_Ringdown/'
     experiment = Experiment(folder_path)
     print(experiment.lsgroups())
+    ringdowns = experiment.loadgroup('PA_50')
+
 
 if __name__ == '__main__':
     main()
